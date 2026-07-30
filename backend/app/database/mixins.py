70cba182b4f1527f9a33772ea9934ecb902ebcfe
depietromarco65 <depietromarco65@gmail.com>
2026-07-30 12:03:46 +0200

    Update mixins.py

--- backend/app/database/mixins.py
+++ backend/app/database/mixins.py
@@ -1,11 +1,20 @@
 from datetime import datetime
 from uuid import uuid4
 
-from sqlalchemy import Boolean, DateTime, Integer
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Boolean
+from sqlalchemy import DateTime
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
 
 
 class BaseMixin:
+    """
+    Campi comuni a tutte le tabelle del database.
+    """
+
     id: Mapped[int] = mapped_column(
         Integer,
         primary_key=True,
@@ -13,23 +22,28 @@ class BaseMixin:
     )
 
     uuid: Mapped[str] = mapped_column(
-        default=lambda: str(uuid4()),
+        String(36),
         unique=True,
+        nullable=False,
+        default=lambda: str(uuid4()),
         index=True,
     )
 
     enabled: Mapped[bool] = mapped_column(
         Boolean,
+        nullable=False,
         default=True,
     )
 
     created_at: Mapped[datetime] = mapped_column(
-        DateTime,
-        default=datetime.utcnow,
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
     )
 
     updated_at: Mapped[datetime] = mapped_column(
-        DateTime,
-        default=datetime.utcnow,
-        onupdate=datetime.utcnow,
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+        onupdate=func.now(),
     )
