87a2e7610dd87e6677f3d0249084fd2bcc807f42
depietromarco65 <depietromarco65@gmail.com>
2026-07-30 07:28:02 +0200

    Create session.py

---
+++ backend/app/database/session.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///vacanze_sicure.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=False,
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
