1414e3cbe8f47224060262aaf9633e46eb07b819
depietromarco65 <depietromarco65@gmail.com>
2026-08-22 23:33:20 +0200

    Load home news highlights independently

--- site/assets/config.js
+++ site/assets/config.js
@@ -7,6 +7,16 @@ window.VS_CONFIG = {
   visiEndpoint: "",
   publicContentEndpoint: ""
 };
+
+(function(){
+  try{
+    var s=document.createElement('script');
+    s.src='assets/home-news.js?v=20260822b';
+    s.defer=true;
+    document.head.appendChild(s);
+  }catch(e){}
+})();
+
 /*
  IMPORTANT:
  - Non inserire service_role, password, API segrete o chiavi private.
